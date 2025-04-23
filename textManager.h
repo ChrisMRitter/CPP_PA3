fdeb6cd4499fced790db5549bad62d3c48e32e5e
AlexanderAldama-Apodaca <130171945+AlexanderAldama-Apodaca@users.noreply.github.com>
2025-04-23 11:55:33 -0400

    Update textManager.h
    Added comments marking what components were done by Alexander Aldama-Apodaca

--- textManager.h
+++ textManager.h
@@ -1,3 +1,7 @@
+/*
+Contributors: Alexander Aldama-Apodaca
+*/
+
 #pragma once
 #ifndef TEXTMANAGER_H
 #define TEXTMANAGER_H
@@ -5,19 +9,26 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 
+// Set up the textManager class - done by Alexander Aldama-Apodaca
 class textManager {
 public:
+	// Constructor - done by Alexander Aldama-Apodaca
 	bool loadFont(const std::string& path);
+	// Function to update the player's health text - done by Alexander Aldama-Apodaca
 	void updatePlayerHealth(int health, sf::Vector2f viewCenter, sf::Vector2f viewSize);
+	// Function to draw the text - done by Alexander Aldama-Apodaca
 	void draw(sf::RenderWindow& window);
+	// Function to add score - done by Alexander Aldama-Apodaca
 	void addScore(int amount);
+	// Function to update the score display - done by Alexander Aldama-Apodaca
 	void updateScoreDisplay(sf::Vector2f viewCenter, sf::Vector2f viewSize);
+	// Function to get the score - done by Alexander Aldama-Apodaca
 	int getScore();
 private:
-	sf::Font font;
-	sf::Text playerHealthText;
-	int score = 0;
-	sf::Text scoreText;
+	sf::Font font; // The font used by the game - done by Alexander Aldama-Apodaca
+	sf::Text playerHealthText; // Text displaying the player's health - done by Alexander Aldama-Apodaca
+	int score = 0; // The player's score, initially set to 0 - done by Alexander Aldama-Apodaca
+	sf::Text scoreText; // Text displaying the player's score - done by Alexander Aldama-Apodaca
 };
 
 #endif
