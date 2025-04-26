5db847bfde696bcb0cebcc06ab8d0a48eb0811b6
AlexanderAldama-Apodaca <130171945+AlexanderAldama-Apodaca@users.noreply.github.com>
2025-04-25 22:53:17 -0400

    Update textManager.h
    Added descriptive comments to textManager.h

--- src/textManager.h
+++ src/textManager.h
@@ -1,5 +1,6 @@
-// The textManager class is intended to provide a singular class to manage the
-// majority of text.
+// textManager.h
+// The textManager class is intended to provide a
+// singular class to manage the majority of text.
 
 #pragma once
 #ifndef TEXTMANAGER_H
@@ -10,12 +11,18 @@
 
 class textManager {
 public:
+  // Function to load font
   bool loadFont(const std::string &path);
+  // Function to update the player's health text
   void updatePlayerHealth(int health, sf::Vector2f viewCenter,
                           sf::Vector2f viewSize);
+  // Function to draw the text
   void draw(sf::RenderWindow &window);
+  // Function to add to the score
   void addScore(int amount);
+  // Function to update the score display
   void updateScoreDisplay(sf::Vector2f viewCenter, sf::Vector2f viewSize);
+  // Function to get the score
   int getScore();
 
 private:
